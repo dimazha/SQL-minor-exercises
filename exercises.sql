c3dfaecca2588424d2f33d8abece830009fdba29
Dmitrii Zharov <106115870+dimazha@users.noreply.github.com>
2022-12-21 05:03:17 +0300

    Create exercises.sql

---
+++ exercises.sql
@@ -0,0 +1,64 @@
+--№1
+SELECT model, speed, hd FROM PC WHERE price < 500
+
+--№2
+SELECT DISTINCT maker FROM Product WHERE Product.type = 'Printer'  
+
+--№3
+SELECT model, ram, screen FROM Laptop WHERE price > 1000  
+
+--№4
+SELECT * FROM Printer WHERE color = 'y'  
+
+--№5
+SELECT model, speed, hd FROM PC WHERE (CD = '12x' OR CD = '24x') AND price < 600  
+
+--№6
+SELECT DISTINCT Product.maker, Laptop.speed
+FROM Laptop
+JOIN Product
+ON Product.model = Laptop.model
+WHERE Laptop.hd >= 10
+
+--№7
+SELECT Product.model, PC.price
+FROM Product 
+JOIN PC
+ON Product.model = PC.model
+WHERE Product.maker = 'B'
+UNION
+SELECT Product.model, Laptop.price
+FROM Product 
+JOIN Laptop
+ON Product.model = Laptop.model
+WHERE Product.maker = 'B'
+UNION
+SELECT Product.model, Printer.price
+FROM Product 
+JOIN Printer
+ON Product.model = Printer.model
+WHERE Product.maker = 'B'
+
+--№8
+SELECT DISTINCT Product.maker
+FROM Product
+WHERE type = 'PC'
+EXCEPT
+SELECT DISTINCT Product.maker
+FROM Product
+WHERE type = 'Laptop'
+
+--№9
+SELECT DISTINCT Product.maker
+FROM PC
+INNER JOIN Product 
+ON Product.model = PC.model
+WHERE PC.speed >= 450
+
+--№10
+SELECT model, price
+FROM Printer
+WHERE price = 
+  (SELECT MAX(price)
+  FROM Printer)
+
