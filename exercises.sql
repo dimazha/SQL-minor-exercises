50c53a19d8c1ff505b4bf220caf046d18c5d64b0
Dmitrii Zharov <106115870+dimazha@users.noreply.github.com>
2022-12-21 05:24:51 +0300

    Update exercises.sql

--- exercises.sql
+++ exercises.sql
@@ -123,3 +123,105 @@ FROM Product
 WHERE type = 'pc'
 GROUP BY maker
 HAVING COUNT(model) >= 3
+
+--№21
+SELECT maker, MAX(PC.price)
+FROM PC, Product
+WHERE PC.model = Product.model
+GROUP BY Product.maker
+
+--№22
+SELECT speed, AVG(price)
+FROM PC
+WHERE speed > 600
+GROUP BY speed
+
+--№23
+SELECT DISTINCT maker
+FROM Product
+JOIN Laptop
+ON Product.model = Laptop.model
+WHERE speed >= 750
+AND maker IN
+(SELECT DISTINCT maker
+FROM Product
+JOIN PC
+ON Product.model = PC.model
+WHERE speed >= 750)
+
+--№24
+SELECT model FROM
+(SELECT DISTINCT model, price FROM laptop WHERE laptop.price = (SELECT MAX(price) FROM laptop)  
+UNION 
+SELECT DISTINCT model, price FROM pc WHERE pc.price = (SELECT MAX(price) FROM pc)  
+UNION 
+SELECT DISTINCT model, price FROM printer WHERE printer.price = (SELECT MAX(price) FROM printer)) as mod1 
+WHERE mod1.price=(SELECT MAX(price) FROM 
+(SELECT DISTINCT price FROM laptop WHERE laptop.price = (SELECT MAX(price) FROM laptop)  
+UNION 
+SELECT DISTINCT price FROM pc WHERE pc.price = (SELECT MAX(price) FROM pc)  
+UNION 
+SELECT DISTINCT price FROM printer WHERE printer.price = (SELECT MAX(price) FROM printer)) as mod2)
+
+--№25
+SELECT DISTINCT maker
+FROM product
+WHERE model IN (SELECT model
+                FROM pc
+                WHERE ram = (SELECT MIN(ram)
+                             FROM pc)
+AND speed = (SELECT MAX(speed)
+             FROM pc
+             WHERE ram = (SELECT MIN(ram)
+                          FROM pc)))
+AND
+maker IN (SELECT maker
+          FROM product
+          WHERE type='printer')
+
+--№26
+SELECT SUM(s.price)/SUM(s.kol) as Средняя_цена 
+FROM
+(SELECT price,1 as kol FROM pc,product
+ WHERE pc.model=product.model AND product.maker='A'
+UNION all
+ SELECT price,1 as kol FROM laptop,product
+ WHERE laptop.model=product.model AND product.maker='A') as s
+
+--№27
+SELECT product.maker, AVG(pc.hd)
+FROM pc, product WHERE product.model = pc.model
+AND product.maker IN (SELECT DISTINCT maker
+FROM product
+WHERE product.type = 'printer')
+GROUP BY maker
+
+--№28
+SELECT COUNT(maker) as mak 
+FROM (SELECT DISTINCT maker
+FROM product 
+GROUP BY maker 
+HAVING COUNT(model) = 1) AS pr
+
+--№29
+SELECT t1.point, t1.date, inc, out
+FROM income_o t1 LEFT 
+JOIN outcome_o t2 ON t1.point = t2.point
+AND t1.date = t2.date
+UNION
+SELECT t2.point, t2.date, inc, out
+FROM income_o t1 RIGHT 
+JOIN outcome_o t2 ON t1.point = t2.point
+AND t1.date = t2.date
+
+--№30
+SELECT point, date, SUM(sum_out), SUM(sum_inc)
+FROM(SELECT point, date, SUM(inc) as sum_inc, null as sum_out 
+     FROM Income 
+     GROUP BY point, date
+UNION
+SELECT point, date, null as sum_inc, SUM(out) as sum_out 
+FROM Outcome 
+GROUP BY point, date ) as t
+GROUP BY point, date 
+ORDER BY point
